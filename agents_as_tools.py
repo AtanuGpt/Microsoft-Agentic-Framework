ef5e1fd5121245b3e37a6a6b5ef68505ad5e5039
AtanuGpt <168507778+AtanuGpt@users.noreply.github.com>
2025-10-31 11:57:42 +0530

    Update agents_as_tools.py

--- agents_as_tools.py
+++ agents_as_tools.py
@@ -1,1 +1,39 @@
+import asyncio
+from agent_framework.azure import AzureOpenAIChatClient
+from azure.identity import AzureCliCredential
+
+instructions_agent01 = "You are an intelligent agent who gets the precise city and country of birth of any cricket player. " \
+                       "Just give the name of the city and country only and nothing else."
+
+instructions_agent02 = "You are a specialized agent in giving expert budget friendly travel itinerary for a given place or country. " \
+                       "Only recommend places, hotels, transport and restaurants suggesations which will in making the plan better; " 
+
+agent01 = AzureOpenAIChatClient(credential=AzureCliCredential()).create_agent(
+    name="CricketerPlaceOfBirth",
+    description="An agent that gets the precise city and country of birth of any cricket player.",
+    instructions=instructions_agent01,
+ )
+
+agent02 = AzureOpenAIChatClient(credential=AzureCliCredential()).create_agent( 
+    name="TravelItineraryAdvisor", 
+    description="An agent that gets can give expert budget friendly travel itinerary for a given place or country",
+    instructions = instructions_agent02
+ )
+
+main_agent = AzureOpenAIChatClient(credential=AzureCliCredential()).create_agent(
+    instructions="You are a helpful assistant. Always use the tools provided to you to get the information you need.",
+    tools=[agent01.as_tool(), agent02.as_tool()]
+)
+
+async def main():
+    message = "Suggest me a 1N/2D travel itinerary for that city of birth of cricketer Rahul Dravid"
+    async for update in main_agent.run_stream(message):
+            if update.text:
+                print(update.text, end="", flush=True)
+
+
+# Run the async function using asyncio
+if __name__ == "__main__":
+    asyncio.run(main())
+
 
