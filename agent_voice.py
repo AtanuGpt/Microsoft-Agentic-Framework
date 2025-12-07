aebe86e2582e0eb59b7402e20b18ed5882abb8b4
AtanuGpt <168507778+AtanuGpt@users.noreply.github.com>
2025-12-07 14:25:06 +0530

    Update agent_voice.py

--- agent_voice.py
+++ agent_voice.py
@@ -1,7 +1,3 @@
-# -------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-# -------------------------------------------------------------------------
 from __future__ import annotations
 import os
 import sys
